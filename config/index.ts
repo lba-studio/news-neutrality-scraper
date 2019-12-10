1a3d3526fda8de497356ea8a6e8e95ef475a8daa
verzac <benjamin.tanone@gmail.com>
2019-12-10 23:47:01 +0700

    add sample .env; add config

---
+++ config/index.ts
@@ -0,0 +1,15 @@
+function getEnv(envVarKey: string): string {
+  let envVar = process.env[envVarKey];
+  if (envVar === undefined) {
+    throw new Error(`Missing environment variable: ${envVarKey}`);
+  }
+  return envVar;
+}
+
+const config = {
+  newsApi: {
+    apiKey: getEnv('NEWS_API_APIKEY'),
+  }
+}
+
+export default config;
