9f2d21ce996a9c54ac666cb0ab628b1e54b9a4b5
verzac <benjamin.tanone@gmail.com>
2019-12-12 16:06:50 +0700

    documented API response from newsApi

--- services/newsapi.service.ts
+++ services/newsapi.service.ts
@@ -2,12 +2,28 @@ import axios, { AxiosResponse } from 'axios';
 import config from '../config';
 import { NewsApiError } from '../errors';
 
+interface NewsApiResponse {
+  status: string;
+  totalResults: number;
+  articles: Array<NewsApiArticle>
+};
+
+interface NewsApiArticle {
+  source: {
+    id?: string;
+    name: string;
+  },
+  author?: string;
+  title: string;
+  description: string;
+  url: string;
+};
+
 const sources = [ // TODO get rid of this
   'abc-news-au',
 ];
 const url = config.newsApi.url;
 const apiKey = config.newsApi.apiKey;
-// const url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=167104b8cc724ae2a0fa9f5ac3ab2ffd';
 
 function handleErrorStatusCode(resp: AxiosResponse): AxiosResponse {
   if (resp.status >= 400) {
@@ -16,7 +32,7 @@ function handleErrorStatusCode(resp: AxiosResponse): AxiosResponse {
   return resp;
 }
 
-export async function getNewsFromSource(source: string) {
+export async function getNewsFromSource(source: string): Promise<NewsApiResponse> {
   return await axios.get(url + '/everything', {
     params: {
       apiKey: apiKey,
