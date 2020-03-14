15d81c94cc31f00369d3b9ed66346d52ebfe9973
verzac <benjamin.tanone@gmail.com>
2020-03-14 22:39:47 +1100

    add plugin to logger.util so that logger.debug actually calls console.debug

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -4,6 +4,13 @@ import { config } from '../config';
 
 // TODO load from env
 const LOG_LEVEL: log.LogLevelDesc = config.logLevel as log.LogLevelDesc;
+const originalFactory = log.methodFactory;
+log.methodFactory = (methodName, logLevel, loggerName) => {
+  if (methodName === 'debug' && logLevel <= log.levels.DEBUG) {
+    return console.debug;
+  }
+  return originalFactory(methodName, logLevel, loggerName);
+};
 log.setDefaultLevel(LOG_LEVEL);
 
 export const logger = log;
