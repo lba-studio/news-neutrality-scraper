f0a0f6a095219e28b63885d400fc4d4ad5c61e7d
verzac <benjamin.tanone@gmail.com>
2020-04-24 22:35:47 +1000

    added /search (search-topic) to search for topics and retrieve their score

---
+++ src/lambda-handlers/search-topic.ts
@@ -0,0 +1,17 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { defaultApiResponseHandler, getBody } from '../helpers/lambda.helper';
+import { BadRequestError } from '../errors';
+import topicService from '../services/topic.service';
+
+
+
+async function searchTopic(event: APIGatewayProxyEvent) {
+  const topic: string | undefined = getBody(event).topic;
+  if (!topic) {
+    throw new BadRequestError('Query parameter topic must be provided.')
+  }
+  const topicScoreResult = await topicService.getTopicScore(topic);
+  return topicScoreResult;
+}
+
+export const handler = defaultApiResponseHandler(searchTopic, 200);
