60b6af93bce1e23d52d2bb48e03469044d63db12
verzac <benjamin.tanone@gmail.com>
2020-04-26 08:02:38 +1000

    added tests for topic.service

---
+++ src/services/topic.service.spec.ts
@@ -0,0 +1,50 @@
+import sinon from 'sinon';
+import topicService, { GetTopicResult } from './topic.service';
+import newsapiService from './news/newsapi.service';
+import sentimentAnalyzerService from './sentiment-analyzer.service';
+import topicScoreRepository from '../repositories/topic-score.repository';
+import { expect } from 'chai';
+
+describe("topic.service", () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
+
+  it('should calculate the average score of each news articles for a particular topic', async () => {
+    const topic = 'huehue';
+    const news = [
+      { content: 'huehue', title: 'title1' },
+      { content: 'wassup', title: 'title2' },
+    ];
+    const expectedResult: GetTopicResult = { score: 0.5, newsArticlesAnalyzed: 2, sampleAnalyzedArticles: news };
+    sinon.stub(newsapiService, 'getNews')
+      .returns(Promise.resolve(news));
+    sinon.stub(sentimentAnalyzerService, 'analyzeText')
+      .onCall(0)
+      .returns(Promise.resolve(1))
+      .onCall(1)
+      .returns(Promise.resolve(0));
+    const putStub = sinon.stub(topicScoreRepository, 'put');
+    const result = await topicService.getTopicScore(topic);
+    expect(putStub.calledOnceWith({ score: 0.5, topic: topic })).to.be.true;
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('should return appropriately when no news could be found', async () => {
+    const topic = 'huehue';
+    const news = [];
+    const expectedResult: GetTopicResult = {
+      score: null,
+      newsArticlesAnalyzed: 0,
+      sampleAnalyzedArticles: []
+    };
+    sinon.stub(newsapiService, 'getNews')
+      .returns(Promise.resolve(news));
+    const analyzeTextStub = sinon.stub(sentimentAnalyzerService, 'analyzeText');
+    const putStub = sinon.stub(topicScoreRepository, 'put');
+    const result = await topicService.getTopicScore(topic);
+    expect(putStub.called).to.be.false;
+    expect(analyzeTextStub.called).to.be.false;
+    expect(result).to.deep.equal(expectedResult);
+  });
+});
